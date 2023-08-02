df9cdc14952a4649b1879a42f9ccd78533699080
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-08-02 07:51:28 +0300

    Update Todos.jsx

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,37 +1,38 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { addTodos } from '../redux/GlobalState'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodos } from '../redux/GlobalState';
+import './todos.css'; // Import the CSS file
 
 const Todos = () => {
-    const [value, setValue] = useState("")
-    const dispatch = useDispatch()
+  const [value, setValue] = useState('');
+  const dispatch = useDispatch();
 
-    const handleSave = (e)=>{
-        e.preventDefault()
-        if(value){
-            dispatch(addTodos({
-            id : Date.now(),
-            title: value,
-            check: undefined
-        }))
-        setValue("")
-        }else{
-            alert("Put some Todos!")
-        }
-
+  const handleSave = (e) => {
+    e.preventDefault();
+    if (value) {
+      dispatch(
+        addTodos({
+          id: Date.now(),
+          title: value,
+          check: undefined,
+        })
+      );
+      setValue('');
+    } else {
+      alert('Put some Todos!');
     }
+  };
+
   return (
-    <div>
-        <div className="todos-input">
-            <form onSubmit={handleSave}>
-                <input type="text" value={value} onChange={(e)=> setValue(e.target.value)}/>
-                <button type='submit'>Add</button>
-            </form>
-
-        </div>
-
+    <div className="todos-container">
+      <div className="todos-input">
+        <form onSubmit={handleSave}>
+          <input type="text" value={value} onChange={(e) => setValue(e.target.value)} placeholder="Add a new todo" />
+          <button type="submit">Add</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todos
+export default Todos;
