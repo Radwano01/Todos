e88792b889a6e2121f6c1907371eec43ee24d059
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-08-02 07:51:55 +0300

    Update TodosCheck.jsx

--- src/components/TodosCheck.jsx
+++ src/components/TodosCheck.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import {removeTodos, setCheck} from "../redux/GlobalState"
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeTodos, setCheck } from '../redux/GlobalState';
+import './todoscheck.css';
 
-const TodosCheck = ({id, title, check}) => {
+const TodosCheck = ({ id, title, check }) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const handleCheck = () => {
+    dispatch(setCheck(id));
+  };
 
-    const handleCheck = ()=>{
-        dispatch(setCheck(id))
-    }
-    const handleRemove = ()=>{
-      dispatch(removeTodos(id))
-    }
+  const handleRemove = () => {
+    dispatch(removeTodos(id));
+  };
+
   return (
-    <div className='inputItem'>
-        <input type="checkbox" id={id}  checked={check} onChange={handleCheck}/>
-        <label htmlFor={id}>{title}</label>
-        <button onClick={handleRemove}>X</button>
+    <div className={`inputItem ${check ? 'checked' : ''}`}>
+      <input type="checkbox" id={id} checked={check} onChange={handleCheck} />
+      <label htmlFor={id}>{title}</label>
+      <button onClick={handleRemove}>X</button>
     </div>
-  )
-}
+  );
+};
 
-export default TodosCheck
+export default TodosCheck;
